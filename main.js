f3fa2de0af5acfbbf0051405bf87cc42d7397af2
hope17ha <n.anastasova91@gmail.com>
2025-09-10 14:46:32 +0300

    UI upgrades and added edit(save/cancel) functionality

--- main.js
+++ main.js
@@ -7,12 +7,12 @@ function loadTasks() {
     tasks = storedTasks ? JSON.parse(storedTasks) : [];
 }
 
-function saveTasks () { 
+function saveTasks() {
     localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
 function update() {
-    const template = html `
+    const template = html`
         <div class="container">
             <h1>To-Do List</h1>
 
@@ -20,22 +20,33 @@ function update() {
             <button @click=${handleAdd} id="addBtn">Add</button>
 
             <ul id="taskList">
-                ${tasks.map((task, index) => html `
+                ${tasks.map((task, index) => html`
                 <li>
-                <div
+                    <div
               style="text-decoration: ${task.completed ? 'line-through' : 'none'}"
+              
             >
+            ${task.completed ? html `<button @click=${() => deleteTask(index)}>Delete</button> `: null}   
               ${task.task}
-</div>
-                    ${!task.completed ? 
-                        html `
-                        <div class="buttons">
-                        <button @click=${() => toggleTaskStatus(index)}>Finish</button>` 
-                        : null}
-                    <button @click=${() => deleteTask(index)}>Delete</button>
                     </div>
+                    ${task.isEditing ?
+            html`
+                        <input id="editInput" type="text" @keydown=${handleKeydown} value="${task.task}" />
+                        <button @click=${() => saveEdit(index)}>Save</button>
+                        <button @click=${() => cancelEdit(index)}>Cancel</button>
+                        ` : null}
+                    ${!task.completed && !task.isEditing ?
+            html`
+                        <div class="buttons">
+                            <button @click=${() => toggleTaskStatus(index)}>Finish</button>
+                            <button @click=${() => deleteTask(index)}>Delete</button>
+                            <button @click=${() => editTask(index)}>Edit</button>`
+            : null}
+                            
+                   
+                        </div>
                 </li>
-                `) }
+                `)}
             </ul>
         </div>
     `
@@ -45,24 +56,24 @@ function update() {
 
 function handleKeydown(e) {
     if (e.key === 'Enter') {
-      handleAdd();
+        handleAdd();
     }
-  }
+}
 
 function handleAdd() {
     const input = document.getElementById('taskInput');
     const task = input.value;
     if (task) {
-        tasks.push({task, completed: false});
+        tasks.push({ task, completed: false, isEditing: false });
         input.value = '';
         saveTasks();
         update();
-    } 
+    }
 }
 
 function toggleTaskStatus(index) {
     tasks[index].completed = !tasks[index].completed;
-   
+
 
     saveTasks();
     update();
@@ -71,6 +82,26 @@ function toggleTaskStatus(index) {
 function deleteTask(index) {
     tasks.splice(index, 1);
     saveTasks();
+    update();
+}
+
+function editTask(index) {
+    tasks[index].isEditing = !tasks[index].isEditing;
+    saveTasks();
+    update();
+
+}
+
+function saveEdit(index) {
+
+    tasks[index].task = document.getElementById('editInput').value;
+    editTask(index)
+    saveTasks();
+    update();
+}
+
+function cancelEdit(index) {
+    tasks[index].isEditing = !tasks[index].isEditing;
     update();
 }
 
